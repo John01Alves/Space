2df5847254de51b28c69eb952af9f6ca7d872b68
John01Alves <johnalvessr@protonmail.com>
2021-06-09 17:41:34 -0300

    game beta v0.12

--- data/levels/mission.py
+++ data/levels/mission.py
@@ -25,7 +25,8 @@ def game():
 
     pygame.mouse.set_visible(False)
 
-    timer = 0
+    timer = recharge = 0
+    shot = 5
 
     while True:
         clock.tick(90)
@@ -33,22 +34,32 @@ def game():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    new_shot = Shot(player_group, shot_group)
-                    new_shot.rect.center = player.rect.center
+            if shot > 0:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        new_shot = Shot(player_group, shot_group)
+                        new_shot.rect.center = player.rect.center
+                        shot -= 1
+                        placar.conte()
         player_group.draw(window)
         timer += 1
         if timer > 30:
             timer = 0
             if random() < 0.3:
                 new_enemy = Meteor(player_group, meteor_group)
+                timer = 0
         collision = pygame.sprite.spritecollide(player, meteor_group, False)
         death = pygame.sprite.groupcollide(shot_group, meteor_group, True, True)
         if collision:
             break
         if death:
             placar.cont()
+        if shot == 0:
+            recharge += 1
+            if recharge == 100:
+                shot = 5
+                recharge = 0
+                placar.recar()
         player_group.update()
         placar.contagem()
         pygame.display.update()
--- data/scoreboard.py
+++ data/scoreboard.py
@@ -5,13 +5,23 @@ class Placar():
     def __init__(self):
         self.window = pygame.display.set_mode([840, 480])
         self.pontos = 0
+        self.short = 5
 
     def cont(self):
         self.pontos += 1
+
+    def conte(self):
+        self.short -= 1
+
+    def recar(self):
+        self.short += 5
 
     def contagem(self):
         fonte = pygame.font.SysFont('arial', 20, False, False)
         text = fonte.render('Pontos = ' + str(self.pontos), True, (255, 255, 255))
         textpos = text.get_rect()
-        self.window.blit(text, textpos)
-        self.window.blit(self.window, (0, 0))
+        self.window.blit(text, (0, 0))
+
+        muni = fonte.render('Munição = ' + str(self.short), True, (255, 255, 255))
+        pos = muni.get_rect()
+        self.window.blit(muni, (150, 0))
